f7259a055f82ada0429917a94dea382ad0b101c9
Victor Ryadovoi <r.v.i.c@yandex.ru>
2021-05-09 15:40:07 +0600

    Initial Commit

---
+++ war-challenge2/war_challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  war_challenge2App.swift
+//  war-challenge2
+//
+//  Created by Victor Ryadovoi on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct war_challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
